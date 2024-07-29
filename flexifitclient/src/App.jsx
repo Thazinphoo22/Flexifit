c9f690e27e5587f8ce7409b9ba75ff9702f7433a
lynn <myatthazinaung225@gmail.com>
2024-07-29 13:22:27 +0800

    Create App.jsx

---
+++ flexifitclient/src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useContext } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import UserContext from "./context/user";
+import Login from "./components/Login";
+import Registration from "./components/Registration";
+import MemberDashboard from "./components/MemberDashboard";
+import StudioDashboard from "./components/StudioDashboard";
+
+const queryClient = new QueryClient();
+
+function App() {
+  const [accessToken, setAccessToken] = useState("");
+  const [showLogin, setShowLogin] = useState(true);
+  const [role, setRole] = useState("");
+  const [loggedInUserRole, setLoggedInUserRole] = useState("");
+  const [email, setEmail] = useState("");
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <UserContext.Provider
+        value={{
+          accessToken,
+          setAccessToken,
+          role,
+          setRole,
+          loggedInUserRole,
+          setLoggedInUserRole,
+        }}
+      >
+        {!accessToken && showLogin && (
+          <Login setEmail={setEmail} setShowLogin={setShowLogin} />
+        )}
+        {!accessToken && !showLogin && (
+          <Registration setShowLogin={setShowLogin} />
+        )}
+        {loggedInUserRole === "member" && accessToken && (
+          <MemberDashboard email={email} />
+        )}
+        {loggedInUserRole === "fitness_studio" && accessToken && (
+          <StudioDashboard email={email} />
+        )}
+      </UserContext.Provider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
